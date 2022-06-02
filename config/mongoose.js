746b704804d700487ba358e44aff1344e40fb30b
Gaurav Khede <gauravkhede1@gmail.com>
2022-06-03 00:20:31 +0530

    mongoAtlas connected

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,14 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/ecommerce_api');
+//mongodb+srv://gaurav:<password>@cluster0.nrbsr.mongodb.net/?retryWrites=true&w=majority
+//gaurav
+//bl5ZNeFyGGwffxXn
+mongoose.connect('mongodb+srv://cluster0.nrbsr.mongodb.net/?retryWrites=true&w=majority/ecommerce_api',{
+    dbName:'ecommerce_api',
+    user:'gaurav',
+    pass:'bl5ZNeFyGGwffxXn'
+});
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error in connecting to database'));
 db.once('open',function(){
     console.log('connection with databse is successful');
-})
+});
